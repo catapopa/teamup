a159134a620545c0240c319a9cc91d073341f50c
shybastian <48442915+shybastian@users.noreply.github.com>
2019-11-26 15:48:14 +0200

    Refactored code, added annotation for attributes to be validated. Caught RuntimeExceptions in code.

--- teamup-server/src/main/java/com/project/teamup/controller/exception/ObjectNotValidException.java
+++ teamup-server/src/main/java/com/project/teamup/controller/exception/ObjectNotValidException.java
@@ -3,6 +3,10 @@ package com.project.teamup.controller.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * This Exception extends the RuntimeException class to use for Spring Validator Annotations.
+ * @author Sebastian
+ */
 @ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class ObjectNotValidException extends RuntimeException {
 
