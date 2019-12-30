ceeec5ca7e9747c328bafd0326687a611a9fdfca
Cata Popa <cataqoq@gmail.com>
2019-12-30 23:16:27 +0200

    user skills.

--- teamup-client/src/app/components/user-details/user-details.component.ts
+++ teamup-client/src/app/components/user-details/user-details.component.ts
@@ -1,58 +1,22 @@
-import { UserService } from 'src/app/core/services/user/user.service';
-import { Component, OnInit, Inject } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { UserSkill } from './../../shared/models/userSkill';
+import { Component, ViewChild, Inject } from '@angular/core';
+import { MatSort, MatTableDataSource, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 
 @Component({
   selector: 'teamup-user-details',
   templateUrl: './user-details.component.html',
   styleUrls: ['./user-details.component.scss']
 })
-export class UserDetailsComponent implements OnInit {
+export class UserDetailsComponent {
 
-  form: FormGroup;
-  id: number;
-  keys: any;
-  originalData: any;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
 
-  constructor(private formBuilder: FormBuilder,
-    private dialogRef: MatDialogRef<UserDetailsComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any,
-    private service: UserService) {
+  dataSource: MatTableDataSource<UserSkill>;
+  displayedColumns: string[] = ['technology'];
 
-  }
-
-  ngOnInit() {
-    this.dialogRef.updateSize('30%', '61%');
-
-    let formData = {};
-    this.id = this.data.id;
-
-    this.getEntryKeys();
-    formData = this.getFormBuilderValues();
-
-    this.form = this.formBuilder.group(
-      formData
-    );
-  }
-
-  getEntryKeys() {
-    this.keys = Object.keys(this.data);
-  }
-
-  getFormBuilderValues() {
-    // get data
-    const formData = {};
-    this.keys.forEach((key: string) => {
-      if (key !== 'id') {
-        return formData[key] = [this.data[key], Validators.required];
-      }
-    });
-    return formData;
-  }
-
-  close() {
-    this.dialogRef.close();
+  constructor(private dialogRef: MatDialogRef<UserDetailsComponent>, @Inject(MAT_DIALOG_DATA) public data: any) {
+    this.dataSource = new MatTableDataSource<UserSkill>(this.data as UserSkill[]);
+    console.log(this.data)
 
   }
 }
--- teamup-client/src/app/components/users/users.component.ts
+++ teamup-client/src/app/components/users/users.component.ts
@@ -1,8 +1,9 @@
-import {Component, ViewChild} from '@angular/core';
-import {MatSort, MatTableDataSource, MatDialog} from '@angular/material';
-import {User} from 'src/app/shared/models/user';
-import {UserService} from 'src/app/core/services/user/user.service';
-import {UserDetailsComponent} from '../user-details/user-details.component';
+import { UserSkill } from './../../shared/models/userSkill';
+import { Component, ViewChild } from '@angular/core';
+import { MatSort, MatTableDataSource, MatDialog } from '@angular/material';
+import { User } from 'src/app/shared/models/user';
+import { UserService } from 'src/app/core/services/user/user.service';
+import { UserDetailsComponent } from '../user-details/user-details.component';
 
 @Component({
     selector: 'teamup-users',
@@ -11,10 +12,10 @@ import {UserDetailsComponent} from '../user-details/user-details.component';
 })
 export class UsersComponent {
 
-    @ViewChild(MatSort, {static: true}) sort: MatSort;
+    @ViewChild(MatSort, { static: true }) sort: MatSort;
 
     dataSource: MatTableDataSource<User>;
-    displayedColumns: string[] = ['firstName', 'lastName', 'email', 'menu'];
+    displayedColumns: string[] = ['firstName', 'lastName', 'email', 'company', 'location', 'menu'];
 
     constructor(private userService: UserService, private userDetails: MatDialog) {
         this.getData();
@@ -22,6 +23,8 @@ export class UsersComponent {
 
     getData() {
         this.userService.getAll().subscribe((data) => {
+            console.log(data)
+
             this.dataSource = new MatTableDataSource<User>(data as User[]);
             this.dataSource.sort = this.sort;
         });
@@ -29,26 +32,6 @@ export class UsersComponent {
 
     applyFilter(filterValue: string) {
         this.dataSource.filter = filterValue.trim().toLowerCase();
-    }
-
-    getDetails(element) {
-        console.log(element);
-        this.userDetails.open(UserDetailsComponent, {
-            data: {
-                id: element.id,
-                firstName: element.firstName,
-                lastName: element.lastName,
-                email: element.email,
-                birthDate: element.birthDate,
-                seniority: element.seniority,
-                company: element.company.name,
-                skills: element.skills,
-                location: element.location,
-                projectExperiences: element.projectExperiences
-            }
-        }).afterClosed().subscribe(() => {
-            this.getData();
-        });
     }
 
     delete(user: User) {
@@ -59,5 +42,14 @@ export class UsersComponent {
             () => {
             }
         );
+    }
+
+    getDetails(element: { skills: UserSkill; }) {
+        console.log(element);
+        this.userDetails.open(UserDetailsComponent, {
+            data: {
+                skills: element.skills
+            }
+        });
     }
 }
